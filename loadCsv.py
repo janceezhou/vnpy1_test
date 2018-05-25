7cf370b282efc9b951d9cfde4ba0a42bb936760d
janceezhou <38761458+janceezhou@users.noreply.github.com>
2018-05-25 10:54:38 +0200

    Create loadCsv.py

---
+++ loadCsv.py
@@ -0,0 +1,14 @@
+# encoding: UTF-8
+
+"""
+导入BitcoinCharts导出的CSV历史数据到MongoDB中
+"""
+
+from vnpy.trader.app.ctaStrategy.ctaBase import TICK_DB_NAME
+from vnpy.trader.app.ctaStrategy.ctaHistoryData import loadBTCCsv
+
+
+if __name__ == '__main__':
+    loadBTCCsv('bitstampUSD.csv', TICK_DB_NAME, 'bitstampUSD')
+
+
